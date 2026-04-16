3704d068dbc62c54f3c1003bc00faf1e8c5e0d05
Vladmir <konjkov.vv@gmail.com>
2026-04-16 19:33:33 +0700

    улучшил стиль

--- x_files/urls.py
+++ x_files/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('', views.home, name='episodes'),
+    path('', views.home, name='characters'),
+    path('', views.home, name='news'),
+    path('', views.home, name='gallery'),
 ]
--- x_files/views.py
+++ x_files/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'latest_episodes': [],
+        'news_items': [],
+        'featured_character': None,
+        'current_year': datetime.now().year,
+    }
+    return render(request, 'home.html', context)
