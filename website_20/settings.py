e899268454f3b2b3234ebb4149f2fb2879d4ecc7
Vladmir <konjkov.vv@gmail.com>
2026-04-16 18:13:51 +0700

    добавил templates

--- website_20/settings.py
+++ website_20/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp',
+    'x_files',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'website_20.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- website_20/urls.py
+++ website_20/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('testapp.urls')),
+    path('', include('x_files.urls')),
     path('admin/', admin.site.urls),
 ]
