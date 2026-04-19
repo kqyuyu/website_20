88c7541810010bb8cef6d09bb16c4bf2af5c6899
Vladmir <konjkov.vv@gmail.com>
2026-04-19 08:35:03 +0700

    добавил музыку

--- website_20/settings.py
+++ website_20/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Дополнительные папки с вашими статическими файлами (опционально)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # если у вас есть общая папка static в корне проекта
+]
